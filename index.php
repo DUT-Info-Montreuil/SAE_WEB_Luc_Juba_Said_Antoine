bf000a9f6df2aa5542d0552a418eead9b7d1681f
XiangLuc <xiangluc01@gmail.com>
2023-12-05 15:26:00 +0100

    ajout du module connexion

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+
+include_once('connexion.php');
+
+$con = new Connexion();
+$con::init_Connexion();
+
+include_once('vue_generique.php');
+
+$vueGen = new VueGenerique;
+
+$module = isset($_GET['module']) ? htmlspecialchars($_GET['module']) : "connexion"; 
+
+switch($module) {
+    case 'connexion':
+        include_once('modules/mod_connexion/mod_connexion.php');
+        $modConnexion = new ModConnexion;
+        $modConnexion->exec();
+    break;
+    default:
+       die("Le module n'existe pas.");
+}
+
+$contenu = $vueGen->getAffichage();
+
+include_once("composants/menu/comp_menu.php");
+
+$compMenu = new CompMenu;
+
+include_once('template.php');
+
+?>
