f0a32aa019549afb0f35b8a1301ea3fce33ca91a
said <saidkamalshinwari098@gmail.com>
2023-12-07 16:15:44 +0100

    liste et détaile des acteurs

---
+++ modules/mod_acteur/modele_acteur.php
@@ -0,0 +1,24 @@
+<?php
+    require_once "connexion.php";
+
+class ModeleActeur extends Connexion{
+    public  function __construct() {
+
+    }
+    public  function  getListe() {
+
+        $stmt = self::$bdd->prepare('SELECT * FROM Acteurs');
+        $stmt->execute();
+        $tabresault=$stmt->fetchall();
+        return $tabresault;
+
+    }
+    public function detail($id){
+        $stmt = self::$bdd->prepare('SELECT * FROM Acteurs WHERE id_acteurs = :id');
+        $stmt->execute(array('id' => $id));
+        $tabresault=$stmt->fetch();
+        return $tabresault;
+
+    }
+    
+}
