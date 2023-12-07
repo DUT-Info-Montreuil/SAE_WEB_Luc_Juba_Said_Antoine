aadd0578690f35ed013d00b59c4963cfb3ada48d
XiangLuc <xiangluc01@gmail.com>
2023-12-07 16:28:42 +0100

    fonction getListePartie qui récupère la liste de partie grace à sa session

--- modules/mod_partie/modele_partie.php
+++ modules/mod_partie/modele_partie.php
@@ -6,6 +6,13 @@ class ModelePartie extends Connexion {
 
     public function __construct(){}
 
+    public function getListePartie() {
+        $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ?");
+        $query->execute(array(htmlentities($_SESSION['login'])));
+        $res = $query->fetchAll();
+        return $res;
+    }
+
 }
 
 ?>
