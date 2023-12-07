6fbbd6d0bfcc02c4d06befae3e8e0efce7ff3552
XiangLuc <xiangluc01@gmail.com>
2023-12-07 16:31:36 +0100

    action pour afficher l'historique

--- modules/mod_partie/cont_partie.php
+++ modules/mod_partie/cont_partie.php
@@ -11,6 +11,11 @@ class ContPartie {
         $this->modele = new ModelePartie;
         $this->vue = new VuePartie;
     }
+
+    public function affiche_historique() {
+        $tab = $this->modele->getListePartie();
+        $this->vue->affiche_partie($tab);
+    }
 }
 
 ?>
--- modules/mod_partie/mod_partie.php
+++ modules/mod_partie/mod_partie.php
@@ -7,12 +7,17 @@ class ModPartie {
     private $action,$cont;
 
     public function __construct(){
-        $this->cont = new ContConnexion;
-        $this->action = isset($_GET['action']) ? $_GET['action'] : ""; 
+        $this->cont = new ContPartie;
+        $this->action = isset($_GET['action']) ? $_GET['action'] : "affiche"; 
     }
 
     public function exec() {
 
+        switch($this->action) {
+            case "affiche":
+                $this->cont->affiche_historique();
+            break;
+        }
     }
     
 }
