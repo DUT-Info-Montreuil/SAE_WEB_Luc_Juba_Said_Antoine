a14f64ecedc6adfabc002cdcdab14c0cc994afaa
XiangLuc <xiangluc01@gmail.com>
2023-12-07 16:28:58 +0100

    affichage de l'historique des parties

--- modules/mod_partie/vue_partie.php
+++ modules/mod_partie/vue_partie.php
@@ -7,6 +7,24 @@ class VuePartie extends VueGenerique {
     public function __construct(){
         parent::__construct();
     }
+
+    public function affiche_partie($tab) {
+
+        ?> 
+        <div class="d-flex justify-content-center">
+            <ol class="list-group list-group-numbered">
+            <?php 
+            foreach($tab as $values) { 
+                echo "<p> Id : " . $values['id_partie'] . "</p>";
+                echo "<p> Nombre vague : " . $values['nombre_vague'] . "</p>";
+                echo "<p> Ennemis tuer : " . $values['ennemis_tuer'] . "</p";
+                echo "<p> Nombre ennemis : " . $values['nombre_ennemis'] . "</p>";
+            } 
+            ?>
+            </ol>
+        </div>
+        <?php
+    }
 }
 
 ?>
