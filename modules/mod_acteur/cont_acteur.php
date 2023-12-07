2f5f9cd941b1d24549665c4eb4235ab9ca0e13ae
said <saidkamalshinwari098@gmail.com>
2023-12-07 16:21:52 +0100

    une méthode de liste

---
+++ modules/mod_acteur/cont_acteur.php
@@ -0,0 +1,24 @@
+<?php
+require_once "modele_acteur.php";
+require_once "vue_acteur.php";
+class ContActeur {
+    private $modele_acteur;
+    private $vue_acteur;
+
+    public function __construct() {
+        $this->modele_acteur = new ModeleActeur();
+        $this->vue_acteur = new VueActeur();
+    }
+    public function liste() {
+        echo"dans liste contActeur";
+        $liste = $this->modele_acteur->getListe();
+        $this->vue_acteur->affiche_liste($liste); 
+        $this->vue_acteur->menu();
+    }
+
+
+
+}
+
+
+?>
