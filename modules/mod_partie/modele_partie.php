21ad7a2ef41cfcf8529761e2c5cd4e6552d09300
XiangLuc <xiangluc01@gmail.com>
2023-12-11 10:35:04 +0100

    fonction getPartie qui récupère une partie  spécifique d'un utilisateur connecté au site

--- modules/mod_partie/modele_partie.php
+++ modules/mod_partie/modele_partie.php
@@ -13,6 +13,13 @@ class ModelePartie extends Connexion {
         return $res;
     }
 
+    public function getPartie($id) {
+        $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ? AND id_partie = ?");
+        $query->execute(array(htmlentities($_SESSION['login']),htmlentities($id)));
+        $res = $query->fetch();
+        return $res;
+    }
+
 }
 
 ?>
