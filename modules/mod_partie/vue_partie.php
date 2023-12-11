5163be8b4752d41e917626100ca903bab612d027
XiangLuc <xiangluc01@gmail.com>
2023-12-11 10:26:40 +0100

    changemement de l'affichage des parties

--- modules/mod_partie/vue_partie.php
+++ modules/mod_partie/vue_partie.php
@@ -9,21 +9,27 @@ class VuePartie extends VueGenerique {
     }
 
     public function affiche_partie($tab) {
-
         ?> 
-        <div class="d-flex justify-content-center">
-            <ol class="list-group list-group-numbered">
+        <div class="d-flex flex-column align-items-center">
+        <h1 class="text-center"> Historique des parties : </h1>
+        <br>
             <?php 
             foreach($tab as $values) { 
-                echo "<p> Id : " . $values['id_partie'] . "</p>";
-                echo "<p> Nombre vague : " . $values['nombre_vague'] . "</p>";
-                echo "<p> Ennemis tuer : " . $values['ennemis_tuer'] . "</p";
-                echo "<p> Nombre ennemis : " . $values['nombre_ennemis'] . "</p>";
+                ?>
+                <div class="list-group">
+                    <a href="index.php?module=partie&action=details&id=<?php echo $values['id_partie'];?>" class="list-group-item list-group-item-action">
+                        <div class="d-flex w-100 justify-content-between">
+                        <h5 class="mb-1">Partie du : </h5>
+                        <small class="text-body-secondary"> <?php echo $values['date']; ?> </small>
+                        </div>
+                        <p class="mb-1"> Cliquez pour voir le détail de la partie.</p>
+                    </a>
+                    </div>
+                <?php
             } 
             ?>
-            </ol>
-        </div>
-        <?php
+    </div>
+    <?php
     }
 }
 
