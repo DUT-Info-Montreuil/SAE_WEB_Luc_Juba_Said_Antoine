ac4dfa015e0eeb910f8599a4f4e8b44d6ebcbb6a
said <saidkamalshinwari098@gmail.com>
2023-12-11 19:30:24 +0100

    affichage des images l'une à coté d'autre

--- modules/mod_acteur/vue_acteur.php
+++ modules/mod_acteur/vue_acteur.php
@@ -6,25 +6,30 @@ class VueActeur extends vueGenerique{
         parent::__construct();
     }
 
-    public function affiche_liste($elements){
-    if (empty($elements)) {
-        echo "<p>Aucun élément à afficher</p>";
-    } else {
-        foreach ($elements as $element) {
-            echo "<br>";
-            // Affiche le nom du joueur comme un lien vers les détails du joueur
-            echo '<a href="index.php?module=mod_acteur&action=details&id=' . $element['id_acteurs'] . '">' . $element['nom'] . '</a>';
+    public function affiche_liste($elements) {
+        if (empty($elements)) {
+            echo "<p>Aucun élément à afficher</p>";
+        } else {
+            echo '<div class="row">'; // Début de la rangée Bootstrap
+    
+            foreach ($elements as $element) {
+                $imageUrl = htmlspecialchars($element['image']);
+                $elementId = htmlspecialchars($element['id_acteurs']); 
+    
+                // Affiche l'image dans une colonne Bootstrap
+                echo '<div class="col">';
+                echo '<a href="index.php?module=mod_acteur&action=details&id=' . $elementId . '">';
+                echo '<img src="' . $imageUrl . '" alt="Image de l\'élément" class="img-fluid">'; // 'img-fluid' pour rendre l'image responsive
+                echo '</a>';
+                echo '</div>';
+            }
+    
+            echo '</div>';
         }
-        echo "<br>";
     }
-}
+    
+    
 
-
-    public function menu(){
-        echo '<div class="menu">';
-        echo '<a href="index.php?module=mod_acteur&action=liste">Liste les acteurs</a>';
-        echo '</div>';
-    }
 
     public function affiche_details_joueur($acteur)
     {
@@ -37,7 +42,7 @@ class VueActeur extends vueGenerique{
             echo "<p>pv : " . $acteur['pv'] . "</p>";
             echo "<p>attaque : " . $acteur['attaque'] . "</p>";
             echo "<p>descriptif : " . $acteur['descriptif'] . "</p>";
-            echo "<p>Logo : <img src='" . $acteur['image'] . "' alt='Logo de " . $acteur['image'] . "' style='max-width:100px;max-height:100px;'></p>";
+            //echo "<p>Logo : <img src='" . $acteur['image'] . "' alt='Logo de " . $acteur['image'] . "' style='max-width:100px;max-height:100px;'></p>";
         }
     }
 
