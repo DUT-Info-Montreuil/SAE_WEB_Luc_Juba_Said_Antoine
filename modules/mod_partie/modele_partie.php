2b6ff55cdac43e5385b9114b680bf4e0a255606a
XiangLuc <xiangluc01@gmail.com>
2023-12-13 13:54:00 +0100

    modification de l'affichage des parties pour l'afficher du plus récent au plus vieux

--- modules/mod_partie/modele_partie.php
+++ modules/mod_partie/modele_partie.php
@@ -7,28 +7,28 @@ class ModelePartie extends Connexion {
     public function __construct(){}
 
     public function getListePartie() {
-        $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ?");
+        $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ? ORDER BY date DESC");
         $query->execute(array(htmlentities($_SESSION['login'])));
         $res = $query->fetchAll();
         return $res;
     }
 
     public function getListePartieJournalier() {
-        $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ? AND DATE(date) = CURDATE()");
+        $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ? AND DATE(date) = CURDATE() ORDER BY date DESC");
         $query->execute(array(htmlentities($_SESSION['login'])));
         $res = $query->fetchAll();
         return $res;
     }
     
     public function getListePartieHebdomadaire() {
-        $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ? AND YEARWEEK(date, 1) = YEARWEEK(CURDATE(), 1)");
+        $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ? AND YEARWEEK(date, 1) = YEARWEEK(CURDATE(), 1) ORDER BY date DESC");
         $query->execute(array(htmlentities($_SESSION['login'])));
         $res = $query->fetchAll();
         return $res;
     }
 
     public function getListePartieMensuelle() {
-        $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ? AND YEAR(date) = YEAR(CURDATE()) AND MONTH(date) = MONTH(CURDATE())");
+        $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ? AND YEAR(date) = YEAR(CURDATE()) AND MONTH(date) = MONTH(CURDATE()) ORDER BY date DESC");
         $pseudo = htmlentities($_SESSION['login']);
         $query->execute(array($pseudo));
         $res = $query->fetchAll();
