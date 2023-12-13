221bf8b2128db377fbb165544dd08cb467fe96f5
XiangLuc <xiangluc01@gmail.com>
2023-12-13 13:46:57 +0100

    fonction qui liste les parties mensuelle

--- modules/mod_partie/modele_partie.php
+++ modules/mod_partie/modele_partie.php
@@ -27,6 +27,14 @@ class ModelePartie extends Connexion {
         return $res;
     }
 
+    public function getListePartieMensuelle() {
+        $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ? AND YEAR(date) = YEAR(CURDATE()) AND MONTH(date) = MONTH(CURDATE())");
+        $pseudo = htmlentities($_SESSION['login']);
+        $query->execute(array($pseudo));
+        $res = $query->fetchAll();
+        return $res;
+    }
+
     public function getPartie($id) {
         $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ? AND id_partie = ?");
         $query->execute(array(htmlentities($_SESSION['login']),htmlentities($id)));
