a21a0f0c91a6933e0270377b5530a98e119482a1
XiangLuc <xiangluc01@gmail.com>
2023-12-13 13:27:03 +0100

    fonction pour avoir la liste des parties journalière

--- modules/mod_partie/modele_partie.php
+++ modules/mod_partie/modele_partie.php
@@ -13,6 +13,13 @@ class ModelePartie extends Connexion {
         return $res;
     }
 
+    public function getListePartieJournalier() {
+        $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ? AND DATE(date) = CURDATE()");
+        $query->execute(array(htmlentities($_SESSION['login'])));
+        $res = $query->fetchAll();
+        return $res;
+    }
+    
     public function getPartie($id) {
         $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ? AND id_partie = ?");
         $query->execute(array(htmlentities($_SESSION['login']),htmlentities($id)));
