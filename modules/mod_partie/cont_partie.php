3554447bb87e7d5ea3b412d8f8d58d3c5ecf76b3
XiangLuc <xiangluc01@gmail.com>
2023-12-13 13:49:02 +0100

    fonction qui affiche la partie journalier,hebdo et mensuelle

--- modules/mod_partie/cont_partie.php
+++ modules/mod_partie/cont_partie.php
@@ -14,6 +14,25 @@ class ContPartie {
 
     public function affiche_historique() {
         $tab = $this->modele->getListePartie();
+        if(!empty($tab)) {
+            $this->vue->affiche_partie($tab);
+        }else {
+            $this->vue->aucune_partie();
+        }
+    }
+
+    public function affiche_historiqueJournalier() {
+        $tab = $this->modele->getListePartieJournalier();
+        $this->vue->affiche_partie($tab);
+    }
+
+    public function affiche_historiqueHedbo() {
+        $tab = $this->modele->getListePartieHebdomadaire();
+        $this->vue->affiche_partie($tab);
+    }
+
+    public function affiche_historiqueMensuelle() {
+        $tab = $this->modele->getListePartieMensuelle();
         $this->vue->affiche_partie($tab);
     }
 
