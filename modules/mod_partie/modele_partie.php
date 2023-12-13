15ce1f3e06c5c55d6f9fb0c194ec92cfea989bf4
XiangLuc <xiangluc01@gmail.com>
2023-12-13 13:30:22 +0100

    fonction pour la liste des parties hebdomadaire

--- modules/mod_partie/modele_partie.php
+++ modules/mod_partie/modele_partie.php
@@ -20,6 +20,13 @@ class ModelePartie extends Connexion {
         return $res;
     }
     
+    public function getListePartieHebdomadaire() {
+        $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ? AND YEARWEEK(date, 1) = YEARWEEK(CURDATE(), 1)");
+        $query->execute(array(htmlentities($_SESSION['login'])));
+        $res = $query->fetchAll();
+        return $res;
+    }
+
     public function getPartie($id) {
         $query = self::$bdd->prepare("SELECT * FROM Partie INNER JOIN Utilisateur ON Partie.id_utilisateur = Utilisateur.id_utilisateur WHERE pseudo = ? AND id_partie = ?");
         $query->execute(array(htmlentities($_SESSION['login']),htmlentities($id)));
