95bbf875210fb971e1587b08befa8b07b866bdba
weDeZed <juba.chabane8@gmail.com>
2024-01-10 15:57:13 +0100

    feedback début corrections + css

--- modules/mod_feedback/cont_feedback.php
+++ modules/mod_feedback/cont_feedback.php
@@ -17,8 +17,7 @@ class ContFeedback {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             // Récupération de l'id_utilisateur depuis la session
             $idUtilisateur = isset($_SESSION['id_utilisateur']) ? $_SESSION['id_utilisateur'] : null;
-
-
+            
             if ($idUtilisateur&&($_GET['action'] == 'inserer' && $_SERVER['REQUEST_METHOD'] === 'POST')) {
                 // Appel de la méthode soumettreFeedback avec les données POST
                 $this->modele->soumettreFeedback($idUtilisateur, $_POST['nom_utilisateur'], $_POST['email'], $_POST['commentaire']);
