947122a75e1254ecccb063e9ebea1e0040f61765
XiangLuc <xiangluc01@gmail.com>
2024-01-14 15:06:47 +0100

    script js qui utilise la librarie JsChart et récupère les données avec de l'Ajax afin d'afficher les graphiques des moyennes comparant les stats du joueur et du stat globale

--- script.js
+++ script.js
@@ -1,31 +1,70 @@
 if (window.location.href.indexOf('index.php?module=joueur') > -1) {
     $(document).ready(function() {
 
-        $.ajax({
-            url: 'index.php?module=joueur&action=moyenneTuer',
-            type: 'GET',
-            dataType: 'json',
-            success: function(data) {
-                const labels = data.statGlobale.map(entry => entry.mois);
-                const dataGlobal = data.statGlobale.map(entry => entry.Moyenne_tuer);
+        let myChart = null;
 
-                const labelJoueur = data.statJoueur.map(entry => entry.mois);
-                const dataJoueur = data.statJoueur.map(entry => entry.Moyenne_tuer_joueur);
+        afficheGraph('Moyenne_tuer');
 
-                const nom = data.nom;
-
-                updateChart(labels, dataGlobal, labelJoueur, dataJoueur,nom);
-            },
-            error: function(error) {
-                console.log('Erreur lors de la récupération des données : ', error);
-            }
+        $('#ennemis-tues').on('click', function() {
+            afficheGraph('Moyenne_tuer');
         });
 
-        function updateChart(labels, dataGlobal, labelJoueur, dataJoueur,nom) {
+        $('#nombre-vague').on('click', function() {
+            afficheGraph('Moyenne_vague');
+        });
+
+        $('#nombre-tours').on('click', function() {
+            afficheGraph('Moyenne_tour');
+        });
+
+        $('#score').on('click', function() {
+            afficheGraph('Moyenne_score');
+        });
+
+        function afficheGraph(type) {
+            $.ajax({
+                url: 'index.php?module=joueur&action=moyenne',
+                type: 'GET',
+                dataType: 'json',
+                success: function(data) {
+                    const labels = data.statGlobale.map(entry => entry.mois);
+                    let dataGlobal, dataJoueur,titre;
+
+                    if (type === 'Moyenne_tuer') {
+                        dataGlobal = data.statGlobale.map(entry => entry.Moyenne_tuer);
+                        dataJoueur = data.statJoueur.map(entry => entry.Moyenne_tuer_joueur);
+                        titre = "Moyenne des ennemis tué";
+                    } else if (type === 'Moyenne_vague') {
+                        dataGlobal = data.statGlobale.map(entry => entry.Moyenne_vague);
+                        dataJoueur = data.statJoueur.map(entry => entry.Moyenne_vague_joueur);
+                        titre = "Moyenne des vagues";
+                    }else if (type === 'Moyenne_tour') {
+                        dataGlobal = data.statGlobale.map(entry => entry.Moyenne_tours);
+                        dataJoueur = data.statJoueur.map(entry => entry.Moyenne_tours_joueur);
+                        titre = "Moyenne des tours posé";
+                    }else if (type === 'Moyenne_score') {
+                        dataGlobal = data.statGlobale.map(entry => entry.Moyenne_score);
+                        dataJoueur = data.statJoueur.map(entry => entry.Moyenne_score_joueur);
+                        titre = "Moyenne des scores";
+                    }
+
+                    updateChart(labels, dataGlobal, dataJoueur, titre);
+                },
+                error: function(error) {
+                    console.log('Erreur lors de la récupération des données : ', error);
+                }
+            });
+        }
+
+        function updateChart(labels, dataGlobal, dataJoueur,titre) {
             const ctx = document.getElementById('myChart');
 
             if (ctx) {
-                const chart = new Chart(ctx, {
+                if (myChart) {
+                    myChart.destroy();
+                }
+
+                myChart = new Chart(ctx, {
                     type: 'bar',
                     data: {
                         labels: labels,
@@ -51,7 +90,7 @@ if (window.location.href.indexOf('index.php?module=joueur') > -1) {
                         plugins: {
                             title: {
                                 display: true,
-                                text: nom
+                                text: titre
                             }
                         },
                         responsive: true,
