dd89c068cb355746f2794f5baaf18fa48c43b531
weDeZed <juba.chabane8@gmail.com>
2024-01-23 15:17:12 +0100

    feedback terminé 3

--- modules/mod_feedback/modele_feedback.php
+++ modules/mod_feedback/modele_feedback.php
@@ -5,16 +5,7 @@ require_once 'mod_feedback.php';
 
 class ModeleFeedback extends Connexion {
     public function insererFeedback() {
-        /*
-        try {
-          
-            $requete = self::$bdd->prepare("INSERT INTO Feedback (id_utilisateur, nom_utilisateur, email, commentaire) VALUES (?, ?, ?, ?)");
-            return $requete->execute(array(htmlentities($_SESSION['login']),htmlentities($_POST('nom_utilisateur')),htmlentities($_POST('email')),htmlentities($_POST('commentaire'))));
-        } catch (Exception $e) {
-            // Gestion des erreurs
-            die('Erreur : ' . $e->getMessage());
-        }
-        */
+    
         $requete = self::$bdd->prepare("INSERT INTO Feedback (id_utilisateur,nom_utilisateur,email,commentaire) VALUES (?,?,?,?)");
         $requete->execute(array($_SESSION['login'],$_POST['nom_utilisateur'],$_POST['email'],$_POST['commentaire']));
     }
