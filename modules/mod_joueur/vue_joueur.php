866e8b9d5c2beda8d20ed991597d3dfbc8a22b15
Juba CHABANE <juba.chabane8@gmail.com>
2024-01-24 14:52:45 +0100

    c'est antoine et non juba, j'ai apportée une modification dans les pages profil et classement pour le rendre responsive

--- modules/mod_joueur/vue_joueur.php
+++ modules/mod_joueur/vue_joueur.php
@@ -107,144 +107,187 @@ class VueJoueur extends VueGenerique {
     public function filtreClassement()
     {
         ?>
-        <h1>Classement des Joueurs</h1>
-        <div class="dropdown">
-            <a class="btn btn-info dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                filtre Classement
-            </a>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="index.php?module=joueur&action=vague">vague</a></li>
-                <li><a class="dropdown-item" href="index.php?module=joueur&action=score">score</a></li>
-            </ul>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h1>Classement des Joueurs</h1>
+                </div>
+            </div>
+            <div class="row justify-content-center mt-4">
+                <div class="col-md-6">
+                    <div class="dropdown text-center">
+                        <a class="btn btn-info dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Filtre Classement
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="index.php?module=joueur&action=vague">Vague</a></li>
+                            <li><a class="dropdown-item" href="index.php?module=joueur&action=score">Score</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
+        <br>
         <?php
     }
+
 
     public function profil($joueur)
     {
         ?>
-
         <body>
-
-            <h1>Profil Utilisateur</h1>
-
-            <form action="index.php?module=joueur&action=sauvegarde" method="post" enctype="multipart/form-data">
-                <label for="username">Pseudo d’utilisateur : </label> <br>
-                <?php echo $joueur[0]["pseudo"]; ?>
-                <br>
-                <br>
-                <label for="profile_pic">Photo de profile :</label><br>
-
-                <img src="<?php echo $joueur[0]['photo_de_profile']; ?>" class="img-thumbnail" alt="image">
-                <div class="input-group mb-3">
-                    <label class="btn btn-primary" for="inputGroupFile01">Modifier</label>
-                    <input type="file" class="form-control" id="inputGroupFile01" name="inputGroupFile01" style="display: none" accept=".png, .jpg, .jpeg">
-                </div>
-                <br>
-
-                <label for="password">Mot de passe :</label><br>
-                <input type="texte" disabled id="password" name="password" value="******">
-                <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">modifier</button>
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">CONFIRMATION</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <div class="container my-5">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-8 col-lg-6">
+                        <h1 class="text-center">Profil Utilisateur</h1>
+                        <form action="index.php?module=joueur&action=sauvegarde" method="post" enctype="multipart/form-data" class="mt-4">
+                            <div class="mb-3">
+                                <label for="username" class="form-label">Pseudo d’utilisateur :</label>
+                                <p><?php echo htmlspecialchars($joueur[0]["pseudo"]); ?></p>
                             </div>
-                            <div class="modal-body">
-                                Vous etes sur le point de modifier votre passe, ete vous sur de vouloir le faire ?
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">QUITTER</button>
-                                <button type="button" class="btn btn-primary" data-bs-target="#exampleModalToggle2"
-                                    data-bs-toggle="modal">OUI</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2"
-                    tabindex="-1">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Modifier mon Mot de Passe</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="mb-3">
-                                    <label for="exampleInputPassword" class="form-label">Ancien Mot de Passe </label>
-                                    <input type="password" class="form-control" id="exampleInputPassword"
-                                        name="exampleInputPassword">
-                                    <label for="exampleInputPassword1" class="form-label">Nouveau Mot de Passe</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1"
-                                        name="exampleInputPassword1">
+    
+                            <div class="text-center mb-3">
+                                <label for="profile_pic" class="form-label">Photo de profil :</label><br>
+                                <img src="<?php echo htmlspecialchars($joueur[0]['photo_de_profile']); ?>" class="img-fluid rounded-circle mb-3" alt="image" style="width: 150px; height: 150px; object-fit: cover;">
+                                <div>
+                                    <label class="btn btn-primary" for="inputGroupFile01">Modifier</label>
+                                    <input type="file" class="form-control" id="inputGroupFile01" name="inputGroupFile01" style="display: none" accept=".png, .jpg, .jpeg">
                                 </div>
                             </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">QUITTER</button>
-                                <input type="submit" class="btn btn-primary" name="modifierMot_De_Passe" value="Sauvegarder">
+    
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Mot de passe :</label>
+                                <input type="text" disabled class="form-control" id="password" name="password" value="******">
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">modifier</button>
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h1 class="modal-title fs-5" id="exampleModalLabel">CONFIRMATION</h1>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    Vous etes sur le point de modifier votre passe, ete vous sur de vouloir le faire ?
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">QUITTER</button>
+                                                    <button type="button" class="btn btn-primary" data-bs-target="#exampleModalToggle2"
+                                                        data-bs-toggle="modal">OUI</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2"
+                                        tabindex="-1">
+                                        <div class="modal-dialog modal-dialog-centered">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Modifier mon Mot de Passe</h1>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div class="mb-3">
+                                                        <label for="exampleInputPassword" class="form-label">Ancien Mot de Passe </label>
+                                                        <input type="password" class="form-control" id="exampleInputPassword"
+                                                            name="exampleInputPassword">
+                                                        <label for="exampleInputPassword1" class="form-label">Nouveau Mot de Passe</label>
+                                                        <input type="password" class="form-control" id="exampleInputPassword1"
+                                                            name="exampleInputPassword1">
+                                                    </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">QUITTER</button>
+                                                    <input type="submit" class="btn btn-primary" name="modifierMot_De_Passe" value="Sauvegarder">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                                </div>
+    
+                            <!-- Modals pour la modification du mot de passe -->
+    
+                            <div class="mb-3">
+                                <label for="description" class="form-label">Description :</label>
+                                <textarea class="form-control" disabled id="description" name="description"><?php echo htmlspecialchars($joueur[0]["description"]); ?></textarea>
                             </div>
-                        </div>
+    
+                            <div class="d-grid gap-2">
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2">Sauvegarder</button>
+                            <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel2" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="exampleModalLabel">CONFIRMATION</h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Voulez vous vraiment sauvegarder les modifications que vous avez apporté sur votre page profile
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">QUITTER</button>
+                                            <input type="submit" class="btn btn-primary" value="Sauvegarder">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>                            
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <br>
-                <br>
-                <label for="description">Description :</label><br>
-                <textarea id="description" disabled name="description"
-                    value="<?php echo $joueur[0]["description"] ?>"></textarea><br><br>
-
-         
-                <a href="index.php"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">QUITTER</button></a>
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2">Sauvegarder</button>
-                <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel2" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">CONFIRMATION</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                Voulez vous vraiment sauvegarder les modifications que vous avez apporté sur votre page profile
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">QUITTER</button>
-                                <input type="submit" class="btn btn-primary" value="Sauvegarder">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </form>
-
+            </div>
+    
+            <!-- Modals pour la confirmation de sauvegarde -->
+    
         </body>
-
         <?php
     }
+    
+            public function affiche3Classement($joueurs)
+                {
+                    ?>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-md-8 text-center">
+                                <h2>Top 3 Classement</h2>
+                                <ul class="list-group">
+                                    <?php
+                                    $nombreAffiche = 0;
+                                    foreach ($joueurs as $j) {
+                                        if ($nombreAffiche < 3) {
+                                            echo "<li class='list-group-item'>" . htmlspecialchars($j['pseudo']) . " - " . htmlspecialchars($j['max_value']) . "</li>";
+                                            $nombreAffiche++;
+                                        }
+                                    }
+                                    ?>
+                                </ul>
+                                <a href='index.php?module=joueur&action=affichePlusClassement' class='btn btn-primary mt-3'>Afficher Plus</a>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                }
 
-    public function affiche3Classement($joueurs)
-    {
-        $nombreAffiche = 0;
-
-        foreach ($joueurs as $j) {
-            if ($nombreAffiche < 3) {
-                echo $j['pseudo'] . "  " . $j['max_value'] . "<br>";
-                $nombreAffiche++;
-            }
+        public function afficheClassement($joueurs)
+        {
+            ?>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-8 text-center">
+                        <h2>Classement Complet</h2>
+                        <ul class="list-group">
+                            <?php
+                            foreach ($joueurs as $j) {
+                                echo "<li class='list-group-item'>" . htmlspecialchars($j['pseudo']) . " - " . htmlspecialchars($j['max_value']) . "</li>";
+                            }
+                            ?>
+                        </ul>
+                        <a href='index.php' class='btn btn-primary mt-3'>Quitter</a>
+                    </div>
+                </div>
+            </div>
+            <?php
         }
-        echo "<a href='index.php?module=joueur&action=affichePlusClassement' class='btn btn-primary'>Afficher Plus</a>";
-    }
-
-    public function afficheClassement($joueurs)
-    {
-
-        foreach ($joueurs as $j) {
-            echo "<br>" . $j['pseudo'] . "  " . $j['max_value'];
-        }
-        echo "<br> <a href='index.php' class='btn btn-primary'>Quitter</a>";
-    }
 
 }
 
